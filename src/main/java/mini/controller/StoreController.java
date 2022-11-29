9d9738a3d40c88c613eb6ab334ecb45326b5aa42
000922 <dlxownswnsdn@gmail.com>
2022-11-29 14:44:48 +0900

    store 컨트롤 서비스

--- src/main/java/mini/controller/StoreController.java
+++ src/main/java/mini/controller/StoreController.java
@@ -1,30 +1,54 @@
 package mini.controller;
 
 import mini.domain.dto.StoreDto;
+import mini.domain.entity.StoreEntity;
 import mini.service.StoreService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.annotation.Resource;
+import java.util.List;
+
 
 @Controller
 @RestController
+@RequestMapping("/store")
 public class StoreController {
+    // 전역변수
+    @Autowired
+    private StoreService storeService;
+    @Autowired
+    private StoreDto storeDto;
+    @Autowired
+    private StoreEntity storeEntity ;
 
 
-     /*@GetMapping("/store")
-    public Resource.getstore(){
-        return new ClassPathResource("/view/index.html");
-    }*/
+    // HTML 반환
+    @GetMapping("/storelist")
+    public Resource setstore(){
+        return new ClassPathResource("view/index.html");
+    }
+    //등록
+
+    @PostMapping("/setstore")
+    public int setstore(StoreDto storeDto){
+        return storeService.setstore(storeDto);
+    }
+
+    // 출력
+    @GetMapping("/getstore")
+    public List<StoreDto> salelist(){
+        return storeService.storelist();
+    }
 
 
 
-   /* @PostMapping("/setstore")
-    public boolean setStore(StoreDto storeDto){
-        System.out.println("확인 : " + StoreDto.toString);
-        return StoreService.setStore(storeDto);
-    }*/
+
+
+
 }
--- src/main/java/mini/service/StoreService.java
+++ src/main/java/mini/service/StoreService.java
@@ -1,7 +1,55 @@
 package mini.service;
 
+import mini.domain.dto.StoreDto;
+import mini.domain.entity.StoreEntity;
+import mini.domain.entity.StoreRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.transaction.Transactional;
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class StoreService {
+
+    @Autowired
+    private StoreRepository storeRepository ;
+    @Autowired
+    private HttpServletRequest  request ;
+    @Autowired
+    private StoreEntity storeEntity ;
+    @Autowired
+    private StoreService storeService ;
+
+    // -- 서비스
+    @Transactional
+    public int setstore(StoreDto storeDto){
+        StoreEntity storeEntity = storeRepository.save(storeDto.toEntity());
+        return storeEntity.getStno();
+    }
+
+    // 출력
+    public List<StoreDto> storelist(){
+        List<StoreEntity> entities = storeRepository.findAll();
+        List<StoreDto> list = new ArrayList<>();
+        for(StoreEntity storeEntity : entities){
+            list.add(storeEntity.toDto());
+        }
+        return list;
+    }
+
+
+
+
+
+
+
+
+
 }
+
+
+
+
